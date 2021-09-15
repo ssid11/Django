e9099c29d5f700f7a46d0b56117c65517e7f6e2c
ssid <evp.alcor@yandex.ru>
2021-09-15 19:40:50 +0300

    Work with urls

--- geekshop/mainapp/urls.py
+++ geekshop/mainapp/urls.py
@@ -14,11 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from mainapp.views import index, products
+from django.urls import path
+from .views import products
 
+app_name = 'products'
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', index, name = 'index'),
-    path('products/', include('mainapp.urls',namespace='mainapp')),
+    path('', products, name = 'index'),
 ]
