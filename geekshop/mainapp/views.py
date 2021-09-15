4b53353d3aedec1054613b7549e8a114e0f916f4
ssid11 <ssid12@yandex.ru>
2021-09-15 15:00:55 +0300

    Add static work

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -10,15 +10,15 @@ def products(request):
                    [{'name':'Худи черного цвета с монограммами adidas Originals',
                      'price':6090,
                      'desc':'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.',
-                     'img':'/static/vendor/img/products/Adidas-hoodie.png',},
+                     'img':'vendor/img/products/Adidas-hoodie.png',},
                     {'name': 'Синяя куртка The North Face',
                      'price':23725,
                     'desc': 'Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель.',
-                    'img':'/static/vendor/img/products/Blue-jacket-The-North-Face.png',},
+                    'img':'vendor/img/products/Blue-jacket-The-North-Face.png',},
                     {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN',
                      'price':3390,
                      'desc': 'Материал с плюшевой текстурой. Удобный и мягкий.',
-                     'img':'/static/vendor/img/products/Brown-sports-oversized-top-ASOS-DESIGN.png',},
+                     'img':'vendor/img/products/Brown-sports-oversized-top-ASOS-DESIGN.png',},
                     ]
             }
     return render(request,'mainapp/products.html',content)
