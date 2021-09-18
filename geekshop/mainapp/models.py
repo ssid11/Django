f96b9572870ec07cd819d4fe1d50521dbd55c690
ssid <evp.alcor@yandex.ru>
2021-09-18 11:26:27 +0300

    Add quantity in product

--- geekshop/mainapp/models.py
+++ geekshop/mainapp/models.py
@@ -14,6 +14,7 @@ class Product(models.Model):
     name = models.CharField(max_length=128, unique=True)
     description = models.TextField(max_length=512, blank=True)
     price = models.DecimalField(max_digits=8,decimal_places=2,default=0)
+    quantity = models.PositiveIntegerField(default=0)
     ing = models.ImageField(upload_to='products_images', blank=True)
 
     def __str__(self):
