533ab83a02007d757c57a22cefbcfc073cfcc6cf
ssid <evp.alcor@yandex.ru>
2021-09-23 10:24:04 +0300

    Make templates for user login an registration

--- geekshop/users/views.py
+++ geekshop/users/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def login(request):
+    content = {'title':'Авторизация', 'col_lg':'col-lg-5', 'href':'register.html', 'action':'Авторизация',
+               'message':'Нужен аккаунт? Зарегистрируйся',
+    }
+    return render(request,'users/login.html', content)
+
+
+def register(request):
+    content = {'title': 'Регистрация', 'col_lg': 'col-lg-7','href':'login.html', 'action': 'Регистрация',
+               'message': 'Уже есть аккаунт? Авторизоваться',
+               }
+    return render(request,'users/login.html', content)
