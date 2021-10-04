19cb22782bc7e47b118f4fad7015b1c36384db59
ssid <evp.alcor@yandex.ru>
2021-10-04 10:11:11 +0300

    Add base template

--- geekshop/admins/urls.py
+++ geekshop/admins/urls.py
@@ -13,20 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
-from mainapp.views import index
+from .views import index
 
+app_name = 'admins'
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', index, name='index'),
-    path('products/', include('mainapp.urls', namespace='mainapp')),
-    path('users/', include('users.urls', namespace='users')),
-    path('baskets/', include('baskets.urls', namespace='baskets')),
-    path('admins/', include('admins.urls', namespace='admins')),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'mainapp',
     'users',
     'baskets',
+    'admins',
 ]
 
 MIDDLEWARE = [
