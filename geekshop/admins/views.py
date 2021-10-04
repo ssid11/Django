478d1702b02c9803b27e5ae44e204f619c8869c2
ssid11 <ssid12@yandex.ru>
2021-10-04 14:55:26 +0300

    same change

--- geekshop/admins/views.py
+++ geekshop/admins/views.py
@@ -1,6 +1,25 @@
+from users.models import User
 from django.shortcuts import render
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
 # Create your views here.
 
 def index(request):
     return render(request,'admins/admin.html', {'title':'административная панель'})
+
+class UserListView(ListView):
+    model = User
+    template_name = 'admins/admin-user-read.html'
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(UserListView, self).get_context_data(**kwargs)
+        context['title'] = 'Админпанель | Пользователи'
+        return context
+
+class UserCreateView(CreateView):
+    pass
+
+class UserUpdateView(UpdateView):
+    pass
+
+class UserDeleteView(DeleteView):
+    pass
