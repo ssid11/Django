34e0940bd2184fba9e62763bdc0be4e232098da0
ssid <evp.alcor@yandex.ru>
2021-10-11 09:52:42 +0300

    Add filter ad paginator

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,6 +1,7 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
 
-from .models import Product
+from .models import Product, ProductCategory
 
 
 # Create your views here.
@@ -9,6 +10,18 @@ def index(request):
     return render(request, 'mainapp/index.html', content)
 
 
-def products(request):
-    products = Product.objects.all()
-    return render(request, 'mainapp/products.html', {'products': products})
+def products(request, category_id=None, page_id=1):
+    categories = ProductCategory.objects.all()
+    products = Product.objects.filter(category_id=category_id) if category_id != None else Product.objects.all()
+    paginator = Paginator(products,per_page=2)
+    try:
+        products_paginator = paginator.page(page_id)
+    except PageNotAnInteger:
+        products_paginator = paginator.page(1)
+    except EmptyPage:
+        products_paginator = paginator.page(paginator.num_pages)
+
+    context ={'categories': categories,}
+    context.update({'products': products_paginator})
+
+    return render(request, 'mainapp/products.html', context)
