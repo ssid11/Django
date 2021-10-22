09d4bf679c8b4df5d6acfa072da350798c10deee
ssid11 <ssid12@yandex.ru>
2021-10-22 14:01:51 +0300

    Working with order

--- geekshop/ordersapp/urls.py
+++ geekshop/ordersapp/urls.py
@@ -1,23 +1,12 @@
-"""geekshop URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.urls import path
-
-from .views import basket_add, basket_remove, basket_edit, add_ajax
+from .views import OrderCreate, OrderList, OrderDelete, order_forming_complete, OrderDetail, OrderUpdate
 
 app_name = 'ordersapp'
 urlpatterns = [
-    path('add_ajax/<int:product_id>/', add_ajax, name='add_ajax'),
+    path('', OrderList.as_view(), name='list'),
+    path('create', OrderCreate.as_view(), name='create'),
+    path('update/<int:pk>/', OrderUpdate.as_view(), name='update'),
+    path('delete/<int:pk>/', OrderDelete.as_view(), name='delete'),
+    path('read/<int:pk>/', OrderDetail.as_view(), name='read'),
+    path('aa/<int:pk>/', OrderList.as_view(), name='forming_complete'),
 ]
--- geekshop/ordersapp/views.py
+++ geekshop/ordersapp/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView, DetailView
+
+
+class OrderList(ListView):
+    pass
+
+class OrderCreate(CreateView):
+    pass
+
+class OrderUpdate(UpdateView):
+    pass
+
+class OrderDelete(DeleteView):
+    pass
+
+class OrderDetail(DetailView):
+    pass
+
+def order_forming_complete(request, pk):
+    pass
+
+
+
