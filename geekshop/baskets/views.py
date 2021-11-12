05dcbfd60429ad2a39a752063b3080e2087f0119
ssid <evp.alcor@yandex.ru>
2021-11-12 16:35:50 +0300

    Add F and Q objects

--- geekshop/baskets/views.py
+++ geekshop/baskets/views.py
@@ -1,8 +1,9 @@
+from django.db import  connection
 from django.http import JsonResponse
 from django.shortcuts import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.template.loader import render_to_string
-
+from django.db.models import F, Q
 from baskets.models import Basket
 from mainapp.models import Product
 
@@ -18,8 +19,11 @@ def basket_add(request, product_id):
         Basket.objects.create(user=user_from_request, product=product, quantity=1)
     else:
         basket = baskets.first()
-        basket.quantity += 1
+        # basket.quantity += 1
+        basket.quantity = F('quantity') + 1
         basket.save()
+        up_qr = list(filter(lambda x: 'UPDATE' in x['sql'], connection.queries))
+        print(f'Basket add query {up_qr}')
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 @login_required
